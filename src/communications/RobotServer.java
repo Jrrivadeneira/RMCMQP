3064212b4f9a3e5e162fc3f73e3afa4e973ca484
jack rivadeneira <jrrivadeneira@wpi.edu>
2016-02-23 09:47:05 -0500

    Server stuff working

--- src/communications/RobotServer.java
+++ src/communications/RobotServer.java
@@ -1,36 +1,30 @@
 package communications;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
 public class RobotServer {
-
-	public RobotServer() {
-		try {
-			ServerSocket s = new ServerSocket(8000);
-			Socket client = s.accept();
-			PrintWriter outbound = new PrintWriter(client.getOutputStream(),
-					true);
-			BufferedReader inbound = new BufferedReader(new InputStreamReader(
-					client.getInputStream()));
-			String ln = "";
-			while ((ln = inbound.readLine()) != null) {
-				System.out.println(ln);
-				if (ln.equals("bye"))
-					break;
+	public static void main(String[] args) throws IOException {
+		int portNumber = 8000;
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(
+						clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						clientSocket.getInputStream()));) {
+			String inputLine;
+			out.println(portNumber + 1);
+			while ((inputLine = in.readLine()) != null) {
+				if (inputLine.startsWith("r")) {
+					out.println("791 235 777 551 682");
+				} else
+					out.println(inputLine);
 			}
-		} catch (IOException ex) {
-			ex.printStackTrace();
+		} catch (IOException e) {
+			System.out
+					.println("Exception caught when trying to listen on port "
+							+ portNumber + " or listening for a connection");
+			System.out.println(e.getMessage());
 		}
 	}
-
-	public static void main(String[] args) {
-		System.out.println("Server Start!");
-		
-	}
-
 }
